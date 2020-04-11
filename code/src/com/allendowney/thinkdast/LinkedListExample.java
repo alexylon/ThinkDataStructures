246ee0ad3cdf7e99b9c54c26bb512f9fd264f4e4
Alexander Alexandrov <alexander@alexandroff.me>
2020-04-11 14:07:34 +0300

    test

--- code/src/com/allendowney/thinkdast/LinkedListExample.java
+++ code/src/com/allendowney/thinkdast/LinkedListExample.java
@@ -13,6 +13,7 @@ public class LinkedListExample {
 		ListNode node1 = new ListNode(1);
 		ListNode node2 = new ListNode(2);
 		ListNode node3 = new ListNode(3);
+		ListNode node4 = new ListNode(4);
 		
 		node1.next = node2;
 		node2.next = node3;
